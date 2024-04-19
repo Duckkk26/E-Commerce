26d04d9e952c5275ce69050b96632e41148483c2
Duckkk <trongduc2003ht@gmail.com>
2024-04-19 14:14:12 +0700

    Product Page

---
+++ front-end/src/Pages/Product.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrumbs from '../Components/Breadcrumbs/Breadcrumbs';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
+
+import './CSS/Product.css'
+
+function Product() {
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((item) => item.id === Number(productId))
+
+  return (
+    <div className='product'>
+      <Breadcrumbs product={product} />
+      <ProductDisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts />
+    </div>
+  )
+}
+
+export default Product
