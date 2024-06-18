79fb554235edffc62d460119c7a5bed5cb65ad1f
Nguyễn Trọng Đức Intern <trongduc2003ht@gmail.com>
2024-06-19 01:02:51 +0700

    feat: Connect API for Paying by VNPAY

--- frontend/src/Components/OrderBottomBar/OrderBottomBar.jsx
+++ frontend/src/Components/OrderBottomBar/OrderBottomBar.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react'
+import axios from 'axios';
 import { ShopContext } from '../../Context/ShopContext'
 import { useLocation, useNavigate } from 'react-router-dom';
 import ModalViewList from '../ModalViewList/ModalViewList';
@@ -8,6 +9,47 @@ function OrderBottomBar({ order, getTotalCost, getTotalOrderItems }) {
   const navigate = useNavigate();
   const location = useLocation();
   const [isViewList, setIsViewList] = useState(false);
+
+  const handleCheckout = async () => {
+    try {
+      let resData;
+
+      await axios.post('http://localhost:4000/order/add', {
+        ...order,
+        address: `${order.address.street}, ${order.address.ward}, ${order.address.district}, ${order.address.province}`
+      }, {
+        headers: {
+          "auth-token": `${localStorage.getItem('auth-token')}`,
+        }
+      })
+        .then(res => {
+          resData = res.data
+        })
+
+
+      if (resData.success) {
+        let orderId = resData.data.orderId
+
+        if (order.payment_modal === 'VNPAY') {
+          const response = await axios.post('http://localhost:4000/pay/vnpay/create_payment_url', {
+            orderId: orderId,
+            amount: getTotalCost(),
+            orderType: 110000
+          })
+          
+          if (response.data && response.data.redirectUrl) {
+            // Chuyển hướng người dùng đến URL thanh toán
+            window.open(response.data.redirectUrl, '_blank');
+          }
+        }
+
+        // Chuyển đến trang Checkout thông báo đặt hàng thành công
+        navigate('/order/checkout')
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div>
@@ -24,7 +66,7 @@ function OrderBottomBar({ order, getTotalCost, getTotalOrderItems }) {
               <button onClick={() => navigate('/order/payment')} className="btn btn-danger">
                   Tiếp tục
               </button> :
-              <button className="btn btn-danger">
+              <button onClick={() => handleCheckout()} className="btn btn-danger">
                   Thanh toán
               </button>
           }
